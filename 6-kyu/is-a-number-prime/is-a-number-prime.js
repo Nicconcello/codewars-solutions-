53697e22cba5231320b127b99e2ca25b2b9b8207
Nicolò Capoccetti <capoccettinicolo7@gmail.com>
2026-07-26 01:35:26 +0200

    Add solution - CodeHub

---
+++ 6-kyu/is-a-number-prime/is-a-number-prime.js
@@ -0,0 +1,19 @@
+​
+function isPrime(num) {
+  if(num > 1) {
+    if(num == 2) return true;
+    if(num % 2 != 0) {
+      for(let i = 3; i*i <= num; i+=2) {
+        if(num % i == 0) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  return false;
+}
+​
